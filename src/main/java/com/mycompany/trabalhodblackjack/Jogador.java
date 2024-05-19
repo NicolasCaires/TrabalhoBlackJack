7908a62b2a1e85ecfa769436ccba6b7b84d4610a
NicolasCaires <nikcsb@hotmail.com>
2024-05-19 18:35:20 -0300

    Update Jogador.java

--- src/main/java/com/mycompany/trabalhodblackjack/Jogador.java
+++ src/main/java/com/mycompany/trabalhodblackjack/Jogador.java
@@ -9,6 +9,35 @@ package com.mycompany.trabalhodblackjack;
  * @author nikcs
  */
 import java.util.ArrayList;
+package com.mycompany.trabalhodblackjack;
+
+import java.util.Scanner;
+
+public class Jogador extends JogadorBase {
+    private Scanner scanner;
+
+    public Jogador(String nome, Scanner scanner) {
+        super(nome);
+        this.scanner = scanner;
+    }
+
+    @Override
+    public boolean querPedirCarta() {
+        System.out.println("Você quer (1) Comprar ou (2) Parar?");
+        int escolha = -1;
+        while (escolha != 1 && escolha != 2) {
+            try {
+                escolha = Integer.parseInt(scanner.nextLine());
+                if (escolha != 1 && escolha != 2) {
+                    System.out.println("Escolha inválida. Digite (1) para Comprar ou (2) para Parar.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número.");
+            }
+        }
+        return escolha == 1;
+    }
+}
 
 public class Jogador {
     private String nome;
