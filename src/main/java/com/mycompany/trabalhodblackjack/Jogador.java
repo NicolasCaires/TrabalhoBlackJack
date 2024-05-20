bcb474b14aebd0e42bc399d54db08bb46c909c05
NicolasCaires <nikcsb@hotmail.com>
2024-05-19 23:48:16 -0300

    Update Jogador.java

--- src/main/java/com/mycompany/trabalhodblackjack/Jogador.java
+++ src/main/java/com/mycompany/trabalhodblackjack/Jogador.java
@@ -1,50 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.trabalhodblackjack;
 
-/**
- *
- * @author nikcs
- */
 import java.util.ArrayList;
-package com.mycompany.trabalhodblackjack;
 
-import java.util.Scanner;
+public abstract class JogadorBase {
+    protected String nome;
+    protected ArrayList<Carta> mao;
+    protected int pontuacao;
 
-public class Jogador extends JogadorBase {
-    private Scanner scanner;
-
-    public Jogador(String nome, Scanner scanner) {
-        super(nome);
-        this.scanner = scanner;
-    }
-
-    @Override
-    public boolean querPedirCarta() {
-        System.out.println("Você quer (1) Comprar ou (2) Parar?");
-        int escolha = -1;
-        while (escolha != 1 && escolha != 2) {
-            try {
-                escolha = Integer.parseInt(scanner.nextLine());
-                if (escolha != 1 && escolha != 2) {
-                    System.out.println("Escolha inválida. Digite (1) para Comprar ou (2) para Parar.");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Entrada inválida. Por favor, digite um número.");
-            }
-        }
-        return escolha == 1;
-    }
-}
-
-public class Jogador {
-    private String nome;
-    private ArrayList<Carta> mao;
-    private int pontuacao;
-
-    public Jogador(String nome) {
+    public JogadorBase(String nome) {
         this.nome = nome;
         this.mao = new ArrayList<>();
         this.pontuacao = 0;
@@ -72,4 +35,6 @@ public class Jogador {
     public String toString() {
         return nome + " com a mão " + mao.toString() + " e pontuação " + pontuacao;
     }
+
+    public abstract boolean querPedirCarta();
 }
