5a50c51c15855ebb08091c8ac5a76135a816fe61
NicolasCaires <nikcsb@hotmail.com>
2024-05-19 23:46:45 -0300

    Update Baralho.java

--- src/main/java/com/mycompany/trabalhodblackjack/Baralho.java
+++ src/main/java/com/mycompany/trabalhodblackjack/Baralho.java
@@ -3,21 +3,11 @@ package com.mycompany.trabalhodblackjack;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Baralho {
-    private ArrayList<Carta> cartas;
+public abstract class Baralho {
+    protected ArrayList<Carta> cartas;
 
     public Baralho() {
         cartas = new ArrayList<>();
-        String[] naipes = {"Copas", "Ouros", "Paus", "Espadas"};
-        String[] valores = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei", "Ás"};
-        int[] pontos = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
-
-        for (String naipe : naipes) {
-            for (int i = 0; i < valores.length; i++) {
-                cartas.add(new Carta(naipe, valores[i], pontos[i]));
-            }
-        }
-
         embaralhar();
     }
 
@@ -31,5 +21,22 @@ public class Baralho {
             return null;
         }
         return cartas.remove(0);
+    }
+}
+
+class BaralhoNormal extends Baralho {
+    public BaralhoNormal() {
+        super();
+        String[] naipes = {"Copas", "Ouros", "Paus", "Espadas"};
+        String[] valores = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei", "Ás"};
+        int[] pontos = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+
+        for (String naipe : naipes) {
+            for (int i = 0; i < valores.length; i++) {
+                cartas.add(new CartaNormal(naipe, valores[i], pontos[i]));
+            }
+        }
+
+        embaralhar();
     }
 }
